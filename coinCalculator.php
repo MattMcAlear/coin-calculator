12d5bb33f8954cf3904a0f1509793d73df5743ca
Matt McAlear <matt@sscorp.com>
2014-02-13 21:26:59 -0500

    - Removed BG and other minor changes

--- coinCalculator.php
+++ coinCalculator.php
@@ -28,83 +28,84 @@ $totalProfit = $Coin->getTotalPrice()
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     </head>
 	<body style="background: none;">
+		<div align="center">
         <h1>Coin Calculator</h1>
-        <form method="post">
-	        <table class="table-striped table-bordered table-hover">
-	        	<tr>
-	        		<th>Hash Rate <small><i>(KH/s)</i></small></th>
-	        		<td><input type="text" class="form-control" name="khs" id="khs" value="<?php echo $_POST['khs']; ?>" placeholder="Not Available Yet" /></td>
-	        	</tr>
-	        	<tr>
-	        		<td colspan="2" align="center">OR</td>
-	        	</tr>
-	        	<tr>
-	        		<th>Coin Quantity <small><i>(Per Day)</i></small></th>
-	        		<td><input type="text" class="form-control" name="qty" id="qty" value="<?php echo $_POST['qty']; ?>" placeholder="1000" /></td>
-	        	</tr>
-	        	<tr>
-	        		<th>Coin Type</th>
-	        		<td>
-	        			<select class="form-control" name="type" id="type">
-							<option value="doge">Doge</option>
-	        			</select>
-	        		</td>
-	        	</tr>
-	        	<tr>
-	        		<th>Coin Mining Marketplace</th>
-	        		<td>
-		        		<select class="form-control" name="miningMarketplace" id="miningMarketplace">
-		        			<option value="dogehouse">Dogehouse</option>
-		        		</select>
-	        		</td>
-	        	</tr>
-	        	<tr>
-	        		<th>Coin Conversion Exchange</th>
-	        		<td>
-		        		<select class="form-control" name="conversionExchange" id="conversionExchange">
-		        			<option value="vircurex">Vicurex</option>
-		        		</select>
-	        		</td>
-	        	</tr>
-	        	<tr>
-	        		<th>Coin Cash Out Exchange</th>
-	        		<td>
-		        		<select class="form-control" name="cashOutExchange" id="cashOutExchange">
-		        			<option value="coinbase">Coinbase</option>
-		        		</select>
-	        		</td>
-	        	</tr>
-	        	<tr>
-	        		<th>Wattage</th>
-	        		<td>
-	        			<input type="text" class="form-control" onkeyup="calculateDailyPowerCost()" name="watt" id="watt" value="<?php echo $_POST['watt']; ?>" placeholder="500" />
-	        		</td>
-	        	</tr>
-	        	<tr>
-	        		<th>$/kWh</th>
-	        		<td>
-	        			<input type="text" class="form-control" onkeyup="calculateDailyPowerCost()" name="dkwh" id="dkwh" value="<?php echo $_POST['dkwh']; ?>" placeholder="0.1" />
-	        		</td>
-	        	</tr>
-	        	<tr>
-	        		<th>Daily Power Cost</th>
-	        		<td><input type="text" class="form-control" name="powerCost" id="powerCost" value="<?php echo $_POST['powerCost']; ?>" readonly  /></td>
-	        	</tr>
-	        	<tr>
-	        		<th>Apply Tax</th>
-	        		<td><input type="checkbox" class="form-control" name="tax" id="tax" <?php if($_POST['tax'] == 'on'){ echo 'checked'; } ?> /></td>
-	        	</tr>
-	        	<tr>
-	        		<td></td>
-	        		<td><input type="submit" class="btn btn-primary"></td>
-	        	</tr>
-	        	<tr>
-	        		<th>TOTAL PROFIT <small><i>(Per Day)</i></small></th>
-	        		<td><input type="text" name="profit" id="profit" value="<?php echo $totalProfit; ?>" /></td>
-	        	</tr>
-	        </table>
-        </form>
-        
+	        <form method="post">
+		        <table class="table-striped table-bordered table-hover">
+		        	<tr>
+		        		<th>Hash Rate <small><i>(KH/s)</i></small></th>
+		        		<td><input type="text" class="form-control" name="khs" id="khs" value="<?php echo $_POST['khs']; ?>" placeholder="Not Available Yet" /></td>
+		        	</tr>
+		        	<tr>
+		        		<td colspan="2" align="center">OR</td>
+		        	</tr>
+		        	<tr>
+		        		<th>Coin Quantity <small><i>(Per Day)</i></small></th>
+		        		<td><input type="text" class="form-control" name="qty" id="qty" value="<?php echo $_POST['qty']; ?>" placeholder="1000" /></td>
+		        	</tr>
+		        	<tr>
+		        		<th>Coin Type</th>
+		        		<td>
+		        			<select class="form-control" name="type" id="type">
+								<option value="doge">Doge</option>
+		        			</select>
+		        		</td>
+		        	</tr>
+		        	<tr>
+		        		<th>Coin Mining Marketplace</th>
+		        		<td>
+			        		<select class="form-control" name="miningMarketplace" id="miningMarketplace">
+			        			<option value="dogehouse">Dogehouse</option>
+			        		</select>
+		        		</td>
+		        	</tr>
+		        	<tr>
+		        		<th>Coin Conversion Exchange</th>
+		        		<td>
+			        		<select class="form-control" name="conversionExchange" id="conversionExchange">
+			        			<option value="vircurex">Vicurex</option>
+			        		</select>
+		        		</td>
+		        	</tr>
+		        	<tr>
+		        		<th>Coin Cash Out Exchange</th>
+		        		<td>
+			        		<select class="form-control" name="cashOutExchange" id="cashOutExchange">
+			        			<option value="coinbase">Coinbase</option>
+			        		</select>
+		        		</td>
+		        	</tr>
+		        	<tr>
+		        		<th>Wattage</th>
+		        		<td>
+		        			<input type="text" class="form-control" onkeyup="calculateDailyPowerCost()" name="watt" id="watt" value="<?php echo $_POST['watt']; ?>" placeholder="500" />
+		        		</td>
+		        	</tr>
+		        	<tr>
+		        		<th>$/kWh</th>
+		        		<td>
+		        			<input type="text" class="form-control" onkeyup="calculateDailyPowerCost()" name="dkwh" id="dkwh" value="<?php echo $_POST['dkwh']; ?>" placeholder="0.1" />
+		        		</td>
+		        	</tr>
+		        	<tr>
+		        		<th>Daily Power Cost</th>
+		        		<td><input type="text" class="form-control" name="powerCost" id="powerCost" value="<?php echo $_POST['powerCost']; ?>" readonly  /></td>
+		        	</tr>
+		        	<tr>
+		        		<th>Apply Tax</th>
+		        		<td><input type="checkbox" class="form-control" name="tax" id="tax" <?php if($_POST['tax'] == 'on'){ echo 'checked'; } ?> /></td>
+		        	</tr>
+		        	<tr>
+		        		<td></td>
+		        		<td><input type="submit" class="btn btn-primary"></td>
+		        	</tr>
+		        	<tr>
+		        		<th>TOTAL PROFIT <small><i>(Per Day)</i></small></th>
+		        		<td><input type="text" class="form-control" name="profit" id="profit" value="<?php echo $totalProfit; ?>" /></td>
+		        	</tr>
+		        </table>
+	        </form>
+		</div>
         <script type="text/javascript">
         	function calculateDailyPowerCost(){
 	        	var w = document.getElementById('watt').value;
